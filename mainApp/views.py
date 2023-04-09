e577ec37d090a495564b1eb406343458a087a938
karmadjango1 <karmadjango@gmail.com>
2023-04-09 09:47:40 +0530

    09-04-2023 bug fix

--- mainApp/views.py
+++ mainApp/views.py
@@ -181,7 +181,7 @@ def placeOrderPage(Request):
 @login_required(login_url='/login/')
 def paymentSuccess(request,rppid,rpoid,rpsid):
     buyer = Buyer.objects.get(username=request.user)
-    check = Checkout.objects.filter(user=buyer)
+    check = Checkout.objects.filter(buyer=buyer)
     check=check[::-1]
     check=check[0]
     check.rppid=rppid
